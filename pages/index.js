cbdb2484369be618623fde9dfeba3844abcdf373
KostuX <77516813+KostuX@users.noreply.github.com>
2024-11-28 17:27:53 +0000

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import DefaultLayout from "../layouts/default";
 import { withIronSessionSsr } from "iron-session/next";
-import { ironOptions } from "../config/session/session_config";
+//import { ironOptions } from "../config/session/session_config";
+
+const ironOptions = {
+  cookieName: "teesstsd",
+  password:"bdae47b7efd823209edb23046c68685d4c92f311742976e145b6b4a7e0b79ed7",
+  cookieOptions: {
+    secure: false,
+    maxAge:3600,
+  },
+
+};
 export default function Home({ data }) {
   return (
     <DefaultLayout>
